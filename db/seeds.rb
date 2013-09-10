ab1140269420201cb2cfe6d3394c4e1581c1d524
Santiago Larrain P <stgo.larrain@gmail.com>
2013-09-10 15:36:31 -0400

    #1111373 Seed file for status, labels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Label.create([{name: 'Administration', color: '#2b72dc'}, {name: 'Analysis', color: '#2babd6'}, 
+	{name: 'Requirement', color: '#ff8533'}, {name: 'Feature', color: '#ffad33'}, 
+	{name: 'Design', color: '#9d2bd6'}, {name: 'Bug', color: '#ff3333'}, {name: 'Testing', color: '#ffff33'},
+	{name: 'Chore', color: '#bceb2f'}, {name: 'Suggestion', color: '#2bd62b'}, {name: 'Learning', color: '#472bd6'},
+	{name: 'Research', color: '#020118'}])
+
+Status.create([{name: 'Backlog'}, {name: 'In progress'}, {name: 'Done'}])

3338a786a20134cc5c8c285e8b16d6bf2798dde3
Javier <javier@ubuntu.ubuntu-domain>
2013-09-12 18:49:08 -0300

    #1108378 handling log in errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_session, :except => [:login, :logout]
+
+  def check_session
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
   def after_sign_in_path_for(resource)
   	projects_path
   end

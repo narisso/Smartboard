234029f3ad02abe4b254ffb7c2ba883d0592e4b0
Santiago Larrain P <stgo.larrain@gmail.com>
2013-09-17 02:03:22 -0400

    Awesome fonts commented for heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Iic21542::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # Change for Heroku deploy: "ActionView::Template::Error ('fontawesome.less' wasn't found)"
   # config.assets.compile = false
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true

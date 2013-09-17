f75ba9c50b4c9d31076f98a3c161ede0e976f16a
Santiago Larrain P <stgo.larrain@gmail.com>
2013-09-17 02:20:50 -0400

    Remove confirmable module from devise for Heroku deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
+  # Taken out for heroku deploy: :confirmable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+         
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :current_password

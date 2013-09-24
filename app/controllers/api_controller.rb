8ed77efd61a7d00d330ff7b80304d8d907a7d529
Joseto <jtmarquinezv@gmail.com>
2013-09-24 11:47:49 -0300

    #1119250 Primera prueba de código

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,14 @@
 class ApiController < ApplicationController
+	def getProyects
+		token = params[:token]
+		us = User.find_by_token(token)
+		@id_user = us.id
+		@proj = ProjectRoleUser.where(:user_id => @id_user)
+		if us
+			#@pr = proj.projects.to_json(:include => :id,:name,:role)
+			render :json => @proj #@pr
+		else
+			render :json => {:error => "Error: There's no user with given token."}
+		end
+	end
 end

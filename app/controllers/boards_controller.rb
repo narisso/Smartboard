89349461dabbd670468ef9ed7471888bca3a133d
niescobar <the_realgunner@hotmail.com>
2013-09-25 09:18:05 -0300

    Bug Parcialmente resuelto

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,6 +2,7 @@ class BoardsController < ApplicationController
 
 	def show
 		@project = Project.find(params[:id])
+		unauthorized! if cannot? :read, @project
 		@statuses = Status.where(:project_id => params[:id]).sort_by{|e| e[:order]}
 		@skip_footer = true
 	end

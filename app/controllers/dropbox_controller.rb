99aa6b226e16149fba038c26dc304604e3458689
Thomas Büchi Sagredo <thombuchi@gmail.com>
2013-09-26 18:08:28 -0300

    #1128439 Dropbox Auth

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -1,8 +1,7 @@
 class DropboxController < ApplicationController
 require 'dropbox_sdk'
 def authorize
-	flow = DropboxOAuth2FlowNoRedirect.new(DROPBOX_APP_KEY, DROPBOX_APP_KEY_SECRET)
- authorize_url = flow.start()
+session[:return_to] ||= request.referer
 dbsession = DropboxSession.new(DROPBOX_APP_KEY, DROPBOX_APP_KEY_SECRET)
 #serialize and save this DropboxSession
 session[:dropbox_session] = dbsession.serialize
@@ -19,10 +18,9 @@ def callback
 dbsession = DropboxSession.deserialize(session[:dropbox_session])
 dbsession.get_access_token #we've been authorized, so now request an access_token
 session[:dropbox_session] = dbsession.serialize
-session.delete :dropbox_session
 flash[:success] = "You have successfully authorized with dropbox."
  
-redirect_to root_path
+redirect_to session[:return_to]
  
 end # end of dropbox_callback action
 

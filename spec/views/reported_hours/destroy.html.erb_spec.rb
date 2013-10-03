2aebf4fbaf001521980afb0c758cb9354cb0b07d
Jorge Torres <ocklurvox@gmail.com>
2013-10-03 10:08:01 -0400

    #1130323 Deleted spec/views, Modified controllers specs, added user_spec with two basic tests

--- spec/views/reported_hours/destroy.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "reported_hours/destroy.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/reported_hours/new.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "reported_hours/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end

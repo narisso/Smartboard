5c57efff6cba65cff64ab156d868cc24a7d47375
Javier <javier@ubuntu.ubuntu-domain>
2013-10-24 12:56:32 -0300

    #1138376 Connected with google, but the account is not recognized

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -224,7 +224,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :google_oauth2, 'A632640567465.apps.googleusercontent.com', 'Eb5A8KadNoCzUPY4esc2Bkk4'
+  config.omniauth :google_oauth2, '632640567465.apps.googleusercontent.com', 'Eb5A8KadNoCzUPY4esc2Bkk4'
   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

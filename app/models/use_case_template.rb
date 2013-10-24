7dd7e93215a2c0dfa8fbb542c14bd3f58104359e
Stgo Larrain <stgo.larrain@gmail.com>
2013-10-24 15:24:53 -0300

    #1147597 Read YAML and parse to create templates

--- app/models/use_case_template.rb
+++ app/models/use_case_template.rb
@@ -12,4 +12,18 @@ class UseCaseTemplate < ActiveRecord::Base
   def data_types
   	options = [['string', 1], ['text', 2], ['integer', 3], ['datetime', 4], ['boolean', 5]]
   end
+
+  def self.create_default project
+  	data = YAML::load_file(File.join(Rails.root, 'config', 'default_use_case_templates.yml'))
+  	puts data
+
+  	data.each do |d|
+  		template = UseCaseTemplate.new
+      template.project = project
+  		template.name = d[1]["name"]
+      template.template_form = d[1]["attributes"].to_s
+      template.save
+  	end
+  end
+
 end

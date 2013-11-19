902fc7d653eb17741f474f0bb52e268a50b83a53
Javier <javier@ubuntu.ubuntu-domain>
2013-11-19 16:32:18 -0300

    Commented new Registration controller methods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
 	skip_before_filter :check_session , :only => [:new, :create]
 
+	#This method is an override of the devise method. It was override, 
+	#because it was needed to set a password for users that didn't have one
+	#because they had login with facebook or google
 	def update
 	    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
    		prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
@@ -39,6 +42,8 @@ class RegistrationsController < Devise::RegistrationsController
 		end
 	end
 
+
+	#This method is for update user attributes without using the current password
 	def update_resource_no_password(resource, params)
   		if params[:current_password].blank?
       		params.delete("current_password")

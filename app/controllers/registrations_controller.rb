ca059b0c341d36ab1cdbd6feed1a87ae595e83ea
Javier <javier@ubuntu.ubuntu-domain>
2013-11-19 16:21:16 -0300

    Completed updating password when user uses oauth

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,48 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  skip_before_filter :check_session , :only => [:new, :create]
+	skip_before_filter :check_session , :only => [:new, :create]
 
- 
+	def update
+	    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+   		prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+   		if params[:current_password].blank?
+
+    	if update_resource_no_password(resource, account_update_params)
+      		if is_navigational_format?
+		        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+		          :update_needs_confirmation : :updated
+		        set_flash_message :notice, flash_key
+		    end
+	      	sign_in resource_name, resource, :bypass => true
+	      	respond_with resource, :location => after_update_path_for(resource)
+	    else
+	      	clean_up_passwords resource
+	      	respond_with resource
+	    end
+
+		else
+
+		if update_resource(resource, account_update_params)
+      		if is_navigational_format?
+		        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+		          :update_needs_confirmation : :updated
+		        set_flash_message :notice, flash_key
+		    end
+	      	sign_in resource_name, resource, :bypass => true
+	      	respond_with resource, :location => after_update_path_for(resource)
+	    else
+	      	clean_up_passwords resource
+	      	respond_with resource
+	    end
+
+		end
+	end
+
+	def update_resource_no_password(resource, params)
+  		if params[:current_password].blank?
+      		params.delete("current_password")
+		end    	
+   		resource.update_attributes(params)
+  	end
 end

6cd457c245e1146fd4e12f43ec1aeaa769daad38
Stgo Larrain <stgo.larrain@gmail.com>
2013-11-23 15:44:18 -0300

    #1192584 Default date

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -4,6 +4,7 @@ function loadGraph(){
 
   if(initial_date.val() != "" && final_date.val() != "")
   {
+    $('#charts_tabs').css("display", "");
     var initial_date = initial_date.val();
     var final_date = final_date.val();
 
@@ -13,13 +14,13 @@ function loadGraph(){
     hierarchicalGraphInit(dom_id, data_path, initial_date, final_date);
 
     $("#graph2").html("");
-    var data_path = "reports_tasks_project";
+    var data_path = "reports_tasks_user";
     var dom_id = "#graph2";
     hierarchicalGraphInit(dom_id, data_path, initial_date, final_date);
 
 
     $("#graph3").html("");
-    var data_path = "reports_tasks_user";
+    var data_path = "reports_tasks_project";
     var dom_id = "#graph3";
     hierarchicalGraphInit(dom_id, data_path, initial_date, final_date);
 
@@ -35,6 +36,8 @@ function loadGraph(){
 chosenSelect(200);
 $('#initial_date').datepicker();
 $('#final_date').datepicker();
+
+loadGraph();
 
 $('#final_date').change(function(){
   loadGraph();

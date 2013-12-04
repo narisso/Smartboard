c3d43609b880895e479966ff2d33776cdf3d8e6c
Javier <javier@ubuntu.ubuntu-domain>
2013-12-04 16:55:17 -0300

    Fixed edit user problem

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,35 +12,35 @@ class RegistrationsController < Devise::RegistrationsController
 	    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
 			prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
-			if params[:current_password].blank?
+		if params[:user][:current_password].blank?
 
-		if update_resource_no_password(resource, account_update_params)
-	  		if is_navigational_format?
-		        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-		          :update_needs_confirmation : :updated
-		        set_flash_message :notice, flash_key
+			if update_resource_no_password(resource, account_update_params)
+		  		if is_navigational_format?
+			        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+			          :update_needs_confirmation : :updated
+			        set_flash_message :notice, flash_key
+			    end
+		      	sign_in resource_name, resource, :bypass => true
+		      	respond_with resource, :location => after_update_path_for(resource)
+		    else
+		      	clean_up_passwords resource
+		      	respond_with resource
 		    end
-	      	sign_in resource_name, resource, :bypass => true
-	      	respond_with resource, :location => after_update_path_for(resource)
-	    else
-	      	clean_up_passwords resource
-	      	respond_with resource
-	    end
 
 		else
 
-		if update_resource(resource, account_update_params)
-	  		if is_navigational_format?
-		        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-		          :update_needs_confirmation : :updated
-		        set_flash_message :notice, flash_key
+			if update_resource(resource, account_update_params)
+		  		if is_navigational_format?
+			        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+			          :update_needs_confirmation : :updated
+			        set_flash_message :notice, flash_key
+			    end
+		      	sign_in resource_name, resource, :bypass => true
+		      	respond_with resource, :location => after_update_path_for(resource)
+		    else
+		      	clean_up_passwords resource
+		      	respond_with resource
 		    end
-	      	sign_in resource_name, resource, :bypass => true
-	      	respond_with resource, :location => after_update_path_for(resource)
-	    else
-	      	clean_up_passwords resource
-	      	respond_with resource
-	    end
 
 		end
 	end
@@ -51,9 +51,9 @@ class RegistrationsController < Devise::RegistrationsController
 	# @param resource [Resource] the resource of the application, i.e., the user
 	# @param params [Params] the parameters to update
 	def update_resource_no_password(resource, params)
-			if params[:current_password].blank?
+		if params[:current_password].blank?
 	  		params.delete("current_password")
 		end    	
-			resource.update_attributes(params)
-		end
+		resource.update_attributes(params)
+	end
 end

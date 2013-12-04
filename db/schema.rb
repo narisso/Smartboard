c1298378fc50e2fa4fa516389253d77e725f7af8
Stgo Larrain <stgo.larrain@gmail.com>
2013-12-04 13:45:09 -0300

    Datepicker to project form

--- db/schema.rb
+++ db/schema.rb
@@ -241,11 +241,11 @@ ActiveRecord::Schema.define(:version => 20131126192707) do
     t.integer  "estimated_hours"
     t.integer  "effective_hours"
     t.integer  "priority"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
     t.integer  "goal_id"
     t.integer  "task_depend_id"
-    t.boolean  "lock"
+    t.boolean  "lock",             :default => false
     t.integer  "use_case_id"
   end
 
